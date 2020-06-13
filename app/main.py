3ecf116c152d103d689a785ac3ab0824a2e26f44
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-06-12 22:45:05 -0400

    Transition to TinyDB over FileIO

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from flask import request, jsonify, render_template
 import json
 from flask import Flask
 from flask_cors import CORS
+from tinydb import TinyDB, Query
+
 
 
 app = flask.Flask(__name__)
@@ -18,10 +20,17 @@ def home():
 # A route to return all of the available entries in our catalog.
 @app.route('/word', methods=['GET'])
 def api_all(): 
-    f = open("output.txt", "r")
-    lst = f.readlines()
+    db = TinyDB('wordify.json')
+    cursor = Query()
+    count = db.search(cursor.counter > 0)[0].get("counter")
+    lst = []
+    while len(lst) == 0:
+        count = count + 1
+        lst = db.get(doc_id=count)
+    word = lst.get("word")
+    print(word)
+    db.remove(cursor.word == word)
+    db.update({'counter': count}, cursor.counter > 0)
     returnlst = []
-    returnlst.append(lst.pop(0).rstrip("\n"))
-    f2 = open("output.txt", "w")
-    f2.writelines(lst)
+    returnlst.append(word)
     return jsonify(returnlst)
