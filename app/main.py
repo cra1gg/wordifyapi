7ba35f61ae7862edc13cb19f04e69cc494f0c64d
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-06-21 20:11:35 -0400

    Update arch to use firebase as backend db

--- app/main.py
+++ app/main.py
@@ -3,15 +3,22 @@ from flask import request, jsonify, render_template
 import json
 from flask import Flask
 from flask_cors import CORS
-from tinydb import TinyDB, Query
 from threading import Thread
-
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 app = Flask('')
 app._static_folder = "templates/static/"
 CORS(app)
+cred = credentials.Certificate("firebasecreds.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://wordify-4eccd.firebaseio.com/"
+})
 
 def run():
+
   app.run(host='0.0.0.0',port=8080)
 
 @app.route('/')
@@ -22,16 +29,18 @@ def home():
 # A route to return all of the available entries in our catalog.
 @app.route('/word', methods=['GET'])
 def api_all(): 
-    db = TinyDB('app/wordify.json')
-    cursor = Query()
-    count = db.search(cursor.counter > 0)[0].get("counter")
-    lst = []
-    while len(lst) == 0:
-        count = count + 1
-        lst = db.get(doc_id=count)
-    word = lst.get("word")
-    db.remove(cursor.word == word)
-    db.update({'counter': count}, cursor.counter > 0)
+    ref = db.reference('/words')
+    snapshot = ref.order_by_key().limit_to_first(1).get()
+    res = list(snapshot.keys())[0] 
+    word = snapshot.get(res).get("word")
+    r = requests.get('https://jsonbox.io/box_12bd84a60a7e10896ec4/?q=s:' + word)
+    while len(r.json()) > 0: 
+        ref2 = db.reference('/words/' + str(res))
+        ref2.delete()
+        snapshot = ref.order_by_key().limit_to_first(1).get()
+        res = list(snapshot.keys())[0] 
+        word = snapshot.get(res).get("word")
+        r = requests.get('https://jsonbox.io/box_12bd84a60a7e10896ec4/?q=s:' + word)
     returnlst = []
     returnlst.append(word)
     return jsonify(returnlst)
